244466c34855c358d5d5cfb3696a42f01fb433b7
Ryan Ho Hip <ST10440709@vcconnect.edu.za>
2024-10-07 17:13:52 +0200

    final commit

--- app/src/main/java/com/example/empoweringthenation/WhereToFindUs.kt
+++ app/src/main/java/com/example/empoweringthenation/WhereToFindUs.kt
@@ -1,6 +1,8 @@
 package com.example.empoweringthenation
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,16 +11,18 @@ class WhereToFindUs : AppCompatActivity() {
 
     private lateinit var btnGoBackToFeesCart: Button
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_where_to_find_us)
 
         btnGoBackToFeesCart = findViewById(R.id.btnGoBackToFeesCart)
 
+
+        // Set OnClickListener for Go Back button
         btnGoBackToFeesCart.setOnClickListener {
             val intent = Intent(this, FeesCalculateCart::class.java)
             startActivity(intent)
         }
-
     }
 }
